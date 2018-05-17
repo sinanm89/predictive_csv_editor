41c39a2219ce78d4da52afa150aba53f1726b767
Sinan M <snn@ihg.com>
2018-05-17 16:34:08 -0400

    almost done

--- create_contract.py
+++ create_contract.py
@@ -1,27 +1,13 @@
 import csv
 import collections
 from reader import read_the_csv_files, list_all_files
-from FINAL_freq_map_backup import prev_freq_map as FREQ_MAP
+from _FIXED_attribute_map import prev_freq_map as FREQ_MAP
 import json
 import uuid
 from datetime import datetime
 
-
-# =====
-
-# there needs to be a collection of
-
-# taxes:[]
-# fees:[]
-
-# added to the hcm attribute yaml file
-
-# update the yaml file
-
-# ======
-
 _input_dict = None
-
+VALUES_ENTERED = {}
 DETAIL_MAP = {}
 
 def dict_reduce(input_dict):
@@ -68,37 +54,36 @@ def get_type(val):
         val = 'boolean'
     else:
         val = '===UNKNOWN==='
-        # import pdb; pdb.set_trace()
     return val
 
-def read_csv_values():
-    csv_dir = 'csv_files/'
-    file_list = list_all_files(csv_dir)
-    updated_freq_map = {}
-    for file_name in file_list:
-        # file_name.split('.')[-1]
-        if not '.csv' in file_name:
-            continue
-        with open(csv_dir + file_name, newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            read_csv_data = list(reader)
+# def read_csv_values():
+#     csv_dir = 'csv_files/'
+#     file_list = list_all_files(csv_dir)
+#     updated_freq_map = {}
+#     for file_name in file_list:
+#         # file_name.split('.')[-1]
+#         if not '.csv' in file_name:
+#             continue
+#         with open(csv_dir + file_name, newline='') as csvfile:
+#             reader = csv.reader(csvfile)
+#             read_csv_data = list(reader)
 
-        for line in read_csv_data:
-            key = line[1]
-            if key == '':
-                continue
-            if FREQ_MAP.get(key) == None:
-                continue
-            if FREQ_MAP.get(key) == '':
-                continue
-            updated_freq_map[key] = {
-                'long_key': FREQ_MAP.get(key, ''),
-                'keys_value': line[2],
-                'val_type': get_type(line[2]),
-                'extras': [],
-            }
+#         for line in read_csv_data:
+#             key = line[1]
+#             if key == '':
+#                 continue
+#             if FREQ_MAP.get(key) == None:
+#                 continue
+#             if FREQ_MAP.get(key) == '':
+#                 continue
+#             updated_freq_map[key] = {
+#                 'long_key': FREQ_MAP.get(key, ''),
+#                 'keys_value': line[2],
+#                 'val_type': get_type(line[2]),
+#                 'extras': [],
+#             }
 
-    return updated_freq_map
+#     return updated_freq_map
 
 def create_contract(payload_hierarchy_map):
     # {a:{
@@ -106,7 +91,6 @@ def create_contract(payload_hierarchy_map):
     #     c:-
     #     d:-
     # }}
-    import pdb; pdb.set_trace()
     write_contract_block(payload_hierarchy_map['hotelinfo'], 'hotelinfo')
     for att_k, att_v in payload_hierarchy_map.items():
         if att_k == '_meta':
@@ -131,15 +115,28 @@ def create_contract_block(value_map, key):
 def last_item_in_tier(value_map):
     return len(value_map.keys()) == 1 and '_meta' in value_map.keys()
 
-def write_contract_block(keys_value, key):
+def set_contract_obj_name(key):
+    global VALUES_ENTERED
+    digit = len(VALUES_ENTERED.get(key, []))
+    if digit == 0:
+        # name = '{name}{digit}'.format(name=name, digit=entered_digit+1)
+        # print(name)
+        VALUES_ENTERED[key] = []
+    # if key == 'location':
+    digit = digit + 1 if digit else ''
+    name = '{key}{digit}Object'.format(key=key, digit=digit)
+    VALUES_ENTERED[key].append(name)
+    return name
 
+def write_contract_block(keys_value, key):
     tab = '  '
     tab_count = 1
     type_block = '{tab}type: object\n{tab}properties:'.format(tab=tab)
     if key[-1] == 's':
         type_block = "{tab}type: array\n{tab}items:".format(tab=tab)
-    contract_block = '{key}Object:\n{type_block}\n'.format(
-        key=key, type_block=type_block
+    contract_obj_name = set_contract_obj_name(key)
+    contract_block = '{key}:\n{type_block}\n'.format(
+        key=contract_obj_name, type_block=type_block
     )
     tab_count += 1
     _tab_count = tab_count
@@ -148,8 +145,8 @@ def write_contract_block(keys_value, key):
         child_block = ''
         if last_item_in_tier(keys_value):
             value_type = keys_value['_meta'].get('val_type', None)
-            contract_block = '{key}Object:\n{tab}type: {type}\n'.format(
-                tab=tab, type=value_type, key=key
+            contract_block = '{key}:\n{tab}type: {type}\n'.format(
+                tab=tab, type=value_type, key=contract_obj_name
             )
         else:
             for child in keys_value.keys():
@@ -193,36 +190,51 @@ def write_payload_hierarchy(payload_map):
     with open('payload_map.json', 'w') as open_file:
         open_file.write(str(payload_map))
 
-def change_facilities(pp_map):
-    o = {}
-    for k, v in pp_map.items():
-        if (
-            'facilities' in [e.lower() for e in v['long_key'].split('.')] or
-            'facilities' in [e.lower() for e in k.split('.')]
-        ):
-            ke = []
-            for e in k.split('.'):
-                if e.lower() == 'hotelcontent':
-                    e = 'hotelInfo'
-                if e.lower() == 'facilities':
-                    continue
-                if e.lower() == 'hotelfacilities':
-                    e = 'facilities'
-                if e.lower() == 'value':
-                    continue
-                ke.append(e)
-            v['long_key'] = '.'.join(ke)
-        o[k] = v
-    with open('NEW_facilities_map.json', 'w') as open_file:
-        open_file.write(str(o))
-    return o
+# def change_facilities(pp_map):
+#     o = {}
+#     for k, v in pp_map.items():
+#         if (
+#             'facilities' in [e.lower() for e in v['long_key'].split('.')] or
+#             'facilities' in [e.lower() for e in k.split('.')]
+#         ):
+#             ke = []
+#             for e in k.split('.'):
+#                 if e.lower() == 'hotelcontent':
+#                     e = 'hotelInfo'
+#                 if e.lower() == 'facilities':
+#                     continue
+#                 if e.lower() == 'hotelfacilities':
+#                     e = 'facilities'
+#                 if e.lower() == 'value':
+#                     continue
+#                 ke.append(e)
+#             v['long_key'] = '.'.join(ke)
+#         o[k] = v
+#     with open('NEW_facilities_map.json', 'w') as open_file:
+#         open_file.write(str(o))
+#     return o
+
+def read_attribute_map(att_map):
+    output = {}
+    for k,v in att_map.items():
+        output[k] = {
+            'long_key': v,
+            'val_type': 'string'
+        }
+    return output
 
 if __name__ == '__main__':
-    updated_freq_map = read_csv_values()
-    # import pdb; pdb.set_trace()
-    updated_freq_map = change_facilities(updated_freq_map)
+    global FREQ_MAP
+    # updated_freq_map = read_csv_values()
+    # updated_freq_map = change_facilities(updated_freq_map)
+    updated_freq_map = read_attribute_map(FREQ_MAP)
     new_payload_hierarchy = dict_reduce(updated_freq_map)
+    with open('new_payload_hierarchy.txt', 'w') as open_file:
+        open_file.write(str(new_payload_hierarchy))
     write_payload_hierarchy(new_payload_hierarchy)
     create_contract(new_payload_hierarchy)
+    for k, v in VALUES_ENTERED.items():
+        if len(v) > 1:
+            print('{0:<40} : {1:>30}'.format(k, str(v)))
 
 
